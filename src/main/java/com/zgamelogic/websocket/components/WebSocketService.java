eacee86f6ef2ae6354093003f0f6b0468fda6aa5
Ben Shabowski <benshabowski@gmail.com>
2025-07-18 21:45:33 -0500

    better service class

--- src/main/java/com/zgamelogic/websocket/components/WebSocketService.java
+++ src/main/java/com/zgamelogic/websocket/components/WebSocketService.java
@@ -1,7 +1,10 @@
 package com.zgamelogic.websocket.components;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zgamelogic.websocket.data.WebSocketAuthorization;
 import com.zgamelogic.websocket.exceptions.WebSocketMessageException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
@@ -10,18 +13,65 @@ import org.springframework.web.socket.WebSocketSession;
 import java.util.HashMap;
 
 @Service
+@Slf4j
 public class WebSocketService {
     private final HashMap<String, WebSocketSession> sessions;
     private final WebSocketAuthorization authorization;
+    private final ObjectMapper objectMapper;
 
-    public WebSocketService(@Nullable WebSocketAuthorization authorization){
+    public WebSocketService(@Nullable WebSocketAuthorization authorization, ObjectMapper objectMapper){
         this.authorization = authorization;
+        this.objectMapper = objectMapper;
         sessions = new HashMap<>();
     }
 
     public void logout(WebSocketSession session){
         if(authorization == null) return;
         authorization.logout(session);
+    }
+
+    public void sendDataWithView(WebSocketSession session, Object data, Class<?> view){
+        try {
+            String returnMessageJson = objectMapper.writerWithView(view).writeValueAsString(data);
+            sendMessage(session, new TextMessage(returnMessageJson));
+        } catch (JsonProcessingException exception){
+            log.error("Unable to send error through websocket. {}", exception.getMessage());
+        }
+    }
+
+    public void sendData(WebSocketSession session, Object data){
+        try {
+            String returnMessageJson = objectMapper.writeValueAsString(data);
+            sendMessage(session, new TextMessage(returnMessageJson));
+        } catch (JsonProcessingException exception){
+            log.error("Unable to send error through websocket. {}", exception.getMessage());
+        }
+    }
+
+    public void sendDataToAttribute(String attribute, Object value, Object data){
+        sessions.values()
+            .stream()
+            .filter(session ->
+                session.getAttributes().containsKey(attribute) &&
+                    session.getAttributes().get(attribute).equals(value)
+            ).forEach(session -> sendData(session, data));
+    }
+
+    public void sendDataToAttributeWithView(String attribute, Object value, Object data, Class<?> view){
+        sessions.values()
+            .stream()
+            .filter(session ->
+                session.getAttributes().containsKey(attribute) &&
+                    session.getAttributes().get(attribute).equals(value)
+            ).forEach(session -> sendDataWithView(session, data, view));
+    }
+
+    public void sendDataToAll(Object data){
+        sessions.values().forEach(webSocketSession -> sendData(webSocketSession, data));
+    }
+
+    public void sendDataToAllWithView(Object data, Class<?> view){
+        sessions.values().forEach(webSocketSession -> sendDataWithView(webSocketSession, data, view));
     }
 
     public void sendMessage(WebSocketSession session, TextMessage message){
